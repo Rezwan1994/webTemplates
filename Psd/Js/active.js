8cef398cb70a0b4f732cc08c0ce829fe4373e7b7
Rezwan1994 <rezwanulhaquerezwan@gmail.com>
2016-10-20 00:37:02 +0600

    Simple_Web_programming
    Nothing Important....

---
+++ Psd/Js/active.js
@@ -0,0 +1,12 @@
+       
+     jQuery(document).ready(function($){
+
+    $(".slider_area").owlCarousel({
+        items: 1,
+        nav: true,
+        dots: false,
+        navText:["<i class='fa fa-angle-left'></i>"," <i class='fa fa-angle-right'></i>"]
+    
+    });
+  
+});
